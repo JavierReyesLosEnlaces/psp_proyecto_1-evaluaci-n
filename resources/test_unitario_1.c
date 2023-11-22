863cf91fb3a5d69ca3d001b8ec51e6ff71adfe77
Santi <santyx3434@gmail.com>
2023-11-22 20:55:18 +0100

    TrabajoProcesos_Santi Primera Modificacion real, creacion del test de cpu10.

--- resources/test_unitario_1.c
+++ resources/test_unitario_1.c
@@ -5,34 +5,45 @@
 #include <sys/types.h>// Para tipos de datos relacionados con llamadas al sistema.
 #include <sys/wait.h>// Para la funcion wait y macros relacionadas.
 
-// -----------------------------------------------------------------------------------------------------------------------------------------   
-// MODIFICACION 1-> Cambiamos el valor de MAX_LINE a 40, ya que es el valor que actualmente usamos 
-// cuando instanciamos *args, mas 1 para dejar un espacio nulo al final del array, evitandonos asi una operacion inecesaria dividir para 2.
-// Con esto tambien tendremos que modificar la longitud del array *args[] a MAX_LINE + 1.
-// SIN MODIFICACION -> #define MAX_LINE 80
-// -----------------------------------------------------------------------------------------------------------------------------------------   
+            
+#define MAX_LINE 80// constante que representa la longitud maxima de la linea de comando.
 
-// constante que representa la longitud maxima de la linea de comando.
-#define MAX_LINE 40
+        //-------------------------------------------------------------------------------------------
+        //MODIFICACION 1 -> Comprobamos si el comando ejecutado es cpu10, dentro del metodo ejecutarComando(),
+        // si lo es, se ejecutara un metodo el cual se llama comprobarcpucommand() y ejecuta un comando para ver cada 3 segundos,los 10 procesos que mas cpu consumen.
+        // Para salir del proceso, se usara ctrl + C, si nos sale el mensaje 'Prueba 2: Pasada' significa que el test se ha realizado correctamente.
+        //-------------------------------------------------------------------------------------------
+
+int comprobarcpucommand(char* comando) {
+
+        printf("Va a dejar la ventana actual (Para salir presione ctrl + C), presiona enter.....");
+        fflush(stdout); // Asegura que el mensaje se imprima antes de la pausa
+        getchar();
+        
+        char newComandoCpu[] = "watch -n 3 \'free; echo; uptime; echo; ps aux --sort=-%cpu | head -n 11; echo; who\'";// Con este comando conseguimos mostrar una pequeña lista de 10 procesos que mas cpu consumen, esta se refrescara cada 3 segundos
+        
+        system(newComandoCpu);// se ejecuta el comando
+
+        return 2;
+}
 
 // Esta función toma un comando como entrada y ejecuta ese comando en un proceso hijo
 int ejecutarComando(char* comando) {
 
+    
+    if (strcmp(comando, "cpu10") == 0){
+        
+        return comprobarcpucommand(comando);
+    }
     // Crea un nuevo proceso hijo utilizando fork() y almacena el resultado en pid
     pid_t pid = fork();
 
     // Si el pid es 0, el codigo dentro de este bloque se ejecuta en el proceso hijo.
     if (pid == 0) {
-        
-        
-        // -----------------------------------------------------------------------------------------------------------------------------------------    
-        // MODIFICACION 1.2 -> debemos modificar el valor de la logitud del array por MAX_LINE + 1  como hemos comentado en la modificacion 1.
-        // SIN MODIFICACION -> char *args[MAX_LINE / 2 + 1];
-        // -----------------------------------------------------------------------------------------------------------------------------------------  
-    
+
         // Array de punteros para almacenar los tokens del comando 
         //(token -> tokens son las palabras individuales que resultan de dividir la cadena de comando utilizando un delimitador (en este caso, el espacio en blanco)).
-        char *args[MAX_LINE + 1];
+        char *args[MAX_LINE / 2 + 1];
 
         // Inicialización del *token con el primer token de la cadena de comando.
         char *token = strtok(comando, " ");
@@ -69,17 +80,20 @@ int ejecutarComando(char* comando) {
 
 int main() {
 
-    // Llama a la funcion ejecutarComando con el comando "ls" y se guarda el resultado en resultado.
-    char comando_ls[] = "find /home/alumno/Escritorio/ -type f -name \"*.txt\" -exec grep -i \"patrón\" {} +";
-    int resultado = ejecutarComando(comando_ls);
+    // guardamos nuestro comando en un array de caracteres, y posteriormente se lo pasamos al metodo ejecutarComando().
+    char comando[] = "cpu10";
+    int resultado = ejecutarComando(comando);
+    //int resultado2 = ejecutarComandoPersonalizado()
 
     // Imprime el resultado de la ejecucion del comando "ls" si la funcion ejecutarComando devuelve 0.
     if (resultado == 0) {
         printf("Prueba 1: Pasada - El comando 'ls' se ejecutó correctamente.\n");
-    } else {
+    } else if(resultado == 1){
 
         printf("Prueba 1: Fallida - Error al ejecutar el comando 'ls'.\n");
+    }else if(resultado == 2){// comprobamos si el resultado es 2, en tal caso quiere decir que nuestra prueba de comando cpu10 se ha efectuado correctamente.
+
+        printf("Prueba 2: Pasada - El comando 'cpu10' se ejecutó correctamente.\n");
     }
     return 0;
 }
-
