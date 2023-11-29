0767d89e4c374ff8f53f51f34a135b04acbaa247
JavierReyesLosEnlaces <javi.reyes.losenlaces@gmail.com>
2023-11-29 16:58:41 +0100

    3 modificaciones en comandos.c

--- resources/comandos/comandos.c
+++ resources/comandos/comandos.c
@@ -4,25 +4,36 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h> 
 
 #define MAX_LINE 80 /* Constante llamada "MAX_LINE" que define la longitud máxima del comando */
+#define HISTORY_SIZE 100
+
+void cincoPrimerosProcesos();
+void mostrarHistorial();
+void loadingBar();
 
 int main()
 {
-    char input[MAX_LINE]; // Es una cadena de caracteres que almacena la entrada del usuario
-    int should_run = 1;   // Variable de control para el bucle principal
+    char input[MAX_LINE];           // Es una cadena de caracteres que almacena la entrada del usuario
+    int should_run = 1;             // Variable de control para el bucle principal
+    char *historial[HISTORY_SIZE];  // Array de cadenas que almacena el historial de comandos introducidos
+    int historial_contador = 0;     // Contador del número de comandos introducidos
 
     while (should_run)
     { // Es como un while(true), un "si no ha concluido"
         printf("MiShell> ");
         fflush(stdout); // Limpia el buffer de salida para asegurar que se imprima el mensaje
-
         fgets(input, MAX_LINE, stdin); // Lee la entrada del usuario desde la consola y lo mete en la variable input
                                        // stdin -> entrada estándar (por teclado)
         input[strlen(input) - 1] = '\0'; 
         // fgets incluye el carácter de nueva línea ('\n') al final de la cadena leída desde la entrada estándar, y luego se elimina en la línea siguiente con input[strlen(input) - 1] = '\0';.
         // input[strlen(input) - 1] = '\0'; -> verifica que el último caracter de la cadena "input" se le asigne el caracter nulo
         // el último caracter es el salto de línea y queremos que sea nulo, por eso lo ponemos como nulo
+
+
+        historial[historial_contador] = strdup(input);
+        historial_contador++;
 
         if (strcmp(input, "exit") == 0) // strcmp(a,b) compara a y b. Si es cierto, "se ha concluido" -> 0
         {
@@ -33,41 +44,86 @@ int main()
             pid_t pid = fork(); // Crea un nuevo proceso hijo
 
             if (pid == 0)
-            {                                     // Código del proceso hijo
-                char *args[MAX_LINE / 2 + 1];     // * es como un puntero       char *args[MAX_LINE / 2 + 1] es como un array de punteros a caracteres de tamaño "MAX_LINE / 2 + 1"
-                char *token = strtok(input, " "); // strtok -> divide la entrada en tokens usando espacios como delimitadores
-                                                  // En otras palabras, descompone una cadena más grande en partes más pequeñas o tokens basándose en ciertos caracteres de separación
-                                                  // En este caso, el caracter de separación es un espacio
-                                                  // si escribes "git init" te divide el git y el init
+            { // Código del proceso hijo
+                char *args[MAX_LINE / 2 + 1];
+                char *token = strtok(input, " ");
                 int i = 0;
-                while (token != NULL) // Mientras el token es distinto de null
+                while (token != NULL)
                 {
-                    args[i] = token; // Almacena cada token como un argumento
+                    args[i] = token;
                     token = strtok(NULL, " ");
                     i++;
                 }
-                args[i] = NULL; // Marca el final de los argumentos con NULL
-                                // args[i] = NULL;: Al final del bucle, se coloca un puntero nulo (NULL) para marcar el final de la lista de argumentos.
-                                // Esto es necesario para que execvp() pueda determinar el final de los argumentos.
+                args[i] = NULL;
 
+                //historial[historial_contador] = NULL; // Ensure the next element is NULL for execvp
+
+                if (strcmp(input, "5pp") == 0)
+                {
+                    cincoPrimerosProcesos();
+                    exit(0); 
+                }
+
+                if (strcmp(input, "historial") == 0)
+                {
+                    printf("\n");
+                    mostrarHistorial(historial, historial_contador);
+                    exit(0); // Exit after the command is executed
+                }
+
+                if (strcmp(input, "lb") == 0)
+                {
+                    loadingBar();
+                    exit(0); // Exit after the command is executed
+                }
+                
                 execvp(args[0], args);
-                // UNA MOVIDA:
-                /*
-                execvp: Es una función de la familia exec en C que se utiliza para ejecutar un programa. La letra 'v' indica que se pasa un vector (array) de argumentos al programa.
-                args[0]: Es el primer elemento del array args, que contiene el nombre del programa que se va a ejecutar.
-                    *En C, el nombre del programa es el primer argumento de la lista de argumentos.
-                args: Es el array de punteros a caracteres que representa los argumentos del programa.
-                Cada elemento en args es un puntero a una cadena de caracteres, y la última entrada en este array debe ser NULL para indicar el final de la lista de argumentos.
-                */
 
-                exit(0); // Sale del proceso hijo
+
+                // The code below this line will only be reached if execvp fails
+                perror("Error in execvp");
+                exit(EXIT_FAILURE);
             }
-            else
-            {               // Código del proceso padre
-                wait(NULL); // Espera a que el proceso hijo termine antes de continuar
-            }
+            wait(NULL);
+
         }
     }
 
     return 0; // Fin del programa
+    
+}
+ 
+void cincoPrimerosProcesos(){
+    printf("5 primeros procesos:\n");
+    char comando[] = "ps -eo pid,cmd | head -6";
+    system(comando);
+}
+
+void mostrarHistorial(char *historial[HISTORY_SIZE], int historial_contador){
+    printf("HISTORIAL DE COMANDOS:\n");
+    for (int i = 0; i < historial_contador; i++)
+    {
+        printf("Comando %d: %s\n", i+1, historial[i]);
+    }
+}
+
+void loadingBar() {
+    char progreso[101] = ""; // Se cambia el tamaño de 100 a 101 para incluir el carácter nulo
+    int porcentaje = 0;
+    int numeroAleatorio;
+
+    srand(time(NULL)); // Inicializar la semilla del generador de números aleatorios
+
+    for (int i = 0; i < 100; i++) {
+        system("clear");
+        numeroAleatorio = rand() % 100 + 1;
+        porcentaje += 1;
+        strcat(progreso, "#");
+
+        printf("CARGANDO: %d%% %s", porcentaje, progreso);
+        fflush(stdout);
+
+        usleep(numeroAleatorio * 2000); // Ajustte de velocidad, convertir milisegundos a microsegundos
+    }
+    printf("\nInstalación completada.\n");
 }
